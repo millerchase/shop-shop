ec26b5c22a48765b8a3f49b9c62454742ea70f13
CMiller <mlr.chase40@gmail.com>
2023-01-16 23:09:08 -0600

    add indexedDB offline capabilities for products list and details pages

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-
-import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
+import ProductItem from '../ProductItem';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -13,13 +13,30 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    // if there's data to be stored
     if (data) {
+      // store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      // also take each product and save it to IndexedDB using the helper function
+      data.products.forEach(product => {
+        idbPromise('products', 'put', product);
+      });
+      // check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then(products => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -31,7 +31,7 @@ export function idbPromise(storeName, method, object) {
     request.onsuccess = function (e) {
       // save a reference of the database to the 'db' variable
       db = request.result;
-      // open a transaction do whatever we pass into 'storeName' (must match one of the object store names)
+      // open a transaction to whatever we pass into 'storeName' (must match one of the object store names)
       tx = db.transaction(storeName, 'readwrite');
       // save a reference to that object store
       store = tx.objectStore(storeName);
